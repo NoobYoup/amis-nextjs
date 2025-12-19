c061d769af1475866857c9adeaf40a904f798f51
NoobYoup <minhtrung86823@gmail.com>
2025-12-19 14:10:50 +0700

    Remove scripts and prisma folders, disable ESLint during builds

--- prisma/migrations/20251123150957_remove_order_from_document_categories/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `order` on the `document_categories` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `document_categories` DROP COLUMN `order`;
